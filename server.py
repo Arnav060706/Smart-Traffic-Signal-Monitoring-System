006fcc7b263713e8dcd603bb90776b1d091779bc
Arnav Sharath <arnavsharath@gmail.com>
2026-03-19 23:22:35 +0530

    Added cleaner console output

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import os
 
 SERVER_IP = "0.0.0.0"
 SERVER_PORT = 5005
@@ -7,16 +8,14 @@ SERVER_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((SERVER_IP, SERVER_PORT))
 
-print("Traffic Monitoring Server Running...")
+print("🚦 Smart Traffic Monitoring Server Started...\n")
 
-intersection_status = {}
+def analyze_congestion(queue, wait):
 
-def analyze_congestion(queue_length, waiting_time):
-
-    if queue_length < 5:
+    if queue < 5 and wait < 10:
         return "NORMAL"
 
-    elif queue_length < 15:
+    elif queue < 15:
         return "MODERATE CONGESTION"
 
     else:
@@ -26,25 +25,41 @@ def analyze_congestion(queue_length, waiting_time):
 while True:
 
     data, addr = sock.recvfrom(65535)
-
     packet = json.loads(data.decode())
-    print(packet)
-    intersection_id = "SILK BOARD"  # Placeholder for intersection ID, can be extracted from packet if available
-    queue_length = (
-    packet["north_queue"] +
-    packet["south_queue"] +
-    packet["east_queue"] +
-    packet["west_queue"])
+
+    north = packet["north_queue"]
+    south = packet["south_queue"]
+    east = packet["east_queue"]
+    west = packet["west_queue"]
+
+    total_queue = north + south + east + west
     waiting_time = packet["waiting_time"]
 
-    status = analyze_congestion(queue_length, waiting_time)
+    status = analyze_congestion(total_queue, waiting_time)
 
-    intersection_status[intersection_id] = packet
+    os.system("cls" if os.name == "nt" else "clear")
 
-    print("Received data from:", intersection_id)
-    print("Queue length:", queue_length)
-    print("Waiting time:", waiting_time)
-    print("Traffic Status:", status)
+    print("🚦 TRAFFIC MONITORING DASHBOARD")
+    print("---------------------------------\n")
+
+    print("Intersection : SILK BOARD CROSS")
+    print("Time         :", packet["timestamp"])
+    print("Vehicles     :", packet["vehicle_count"])
+    print()
+
+    print("Queue Lengths")
+    print("North :", north)
+    print("South :", south)
+    print("East  :", east)
+    print("West  :", west)
+    print()
+
+    print("Total Queue  :", total_queue)
+    print("Waiting Time :", waiting_time)
+    print("Signal State :", packet["signal_state"])
+    print()
+
+    print("🚦 Traffic Status :", status)
 
     if status == "SEVERE CONGESTION":
-        print("🚨 ALERT: Severe congestion detected!")
+        print("\n🚨 ALERT: Severe congestion detected!")
